edbad756404365ca4c7999d59078a5a576300006
Heldner, Celine <Celine.Heldner@zuehlke.com>
2023-12-13 21:13:54 +0100

    include SwiftUI inside Jetpack Compose

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,20 +2,52 @@ import UIKit
 import SwiftUI
 import shared
 
-struct ComposeView: UIViewControllerRepresentable {
+// struct ComposeView: UIViewControllerRepresentable {
+//     func makeUIViewController(context: Context) -> UIViewController {
+//         Main_iosKt.MainViewController()
+//     }
+//
+//     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+// }
+
+struct ComposeViewControllerRepresentable: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
-        Main_iosKt.MainViewController()
+        Main_iosKt.MainViewController(createUIView: { () -> UIView in
+            SwiftUIInUIView(
+                content: VStack {
+                    Text("SwiftUI in Compose Multiplatform")
+                }
+            )
+        })
     }
 
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+    }
+}
+
+private class SwiftUIInUIView<Content: View>: UIView {
+
+    init(content: Content) {
+        super.init(frame: CGRect())
+        let hostingController = UIHostingController(rootView: content)
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(hostingController.view)
+        NSLayoutConstraint.activate([
+            hostingController.view.topAnchor.constraint(equalTo: topAnchor),
+            hostingController.view.leadingAnchor.constraint(equalTo: leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: trailingAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 struct ContentView: View {
     var body: some View {
-        ComposeView()
-                .ignoresSafeArea(.all, edges: .bottom) // Compose has own keyboard handler
+        ComposeViewControllerRepresentable()
+                    .ignoresSafeArea(.all)
     }
 }
-
-
-
--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -1,5 +1,39 @@
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.interop.UIKitView
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.UIKit.UIView
+import platform.UIKit.UIViewController
+
 
 actual fun getPlatformName(): String = "iOS"
 
-fun MainViewController() = ComposeUIViewController { CrackCaptureEntryScreen() }
+
+@OptIn(ExperimentalForeignApi::class)
+fun MainViewController(createUIView: () -> UIView): UIViewController =
+    ComposeUIViewController {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .windowInsetsPadding(WindowInsets.systemBars),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            UIKitView(
+                factory = createUIView,
+                modifier = Modifier.size(300.dp)
+                    .border(2.dp, androidx.compose.ui.graphics.Color.Blue),
+            )
+            //CrackCaptureEntryScreen()
+            Text("How to use SwiftUI inside Compose Multiplatform")
+        }
+    }
